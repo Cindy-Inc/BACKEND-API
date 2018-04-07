f21fd1cc1635167296c7097d2fb2923231930e03
Leonardo Furnielis <25433997+lfurnielis@users.noreply.github.com>
2018-04-07 19:59:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const logger = require('./logger/index');
-const { empresa } = require('./routes/empresa/index');
+const { estabelecimento } = require('./routes/estabelecimento/index');
 const { mensagem } = require('./routes/mensagem/index');
 
 app.use(cors());
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
-app.use('/api/empresa', empresa);
+app.use('/api/estabelecimento', estabelecimento);
 app.use('/api/mensagem', mensagem);
 
 app.use((req, res, next) => {
