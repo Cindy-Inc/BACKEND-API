ffe11d3a9ea26556224537fe9ef7bae851087577
Leonardo Furnielis <leofur@br.ibm.com>
2018-04-07 13:26:43 -0300

    estabelecimento DAO

--- dao/estabelecimentoDAO.js
+++ dao/estabelecimentoDAO.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const EstabelecimentoSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        minlength: 8
+    }
+});
+
+
+const Estabelecimento = mongoose.model('Estabelecimento', EstabelecimentoSchema);
+
+module.exports = { Estabelecimento };
