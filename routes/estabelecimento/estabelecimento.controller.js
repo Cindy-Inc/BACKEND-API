8d5a394c8b6859bc4dcc6b7b40622026ac325bcd
Leonardo Furnielis <leonardo.furnielis1@ibm.com>
2018-04-14 10:59:08 -0300

    estabelecimento route complete

--- routes/estabelecimento/estabelecimento.controller.js
+++ routes/estabelecimento/estabelecimento.controller.js
@@ -1,7 +1,25 @@
 'use strict';
 
+const _ = require('lodash');
+const { ObjectID } = require('mongodb');
+const { Estabelecimento } = require('./../../dao/estabelecimentoDAO');
+
 const saveEstabelecimento = (req, res) => {
     res.send({ errorMessage: false, message: 'Estabelecimento work\'s' });
+    let body = _.pick(req.body, ['_user', 'name', 'type', 'address', 'complement',
+        'zip', 'state', 'city', 'phone', 'cnpj']);
+
+    if (!ObjectID.isValid(body._user))
+        return res.status(400).send({ errorMessage: 'Valid _user id is required' });
+
+    let estabelecimento = new Estabelecimento(body);
+
+    estabelecimento.save().then((user) => {
+        res.send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+
 };
 
 module.exports = { saveEstabelecimento };
