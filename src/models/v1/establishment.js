7e2798110303fa9fd119beb7be03efd37412953e
Leonardo Furnielis <leonardo.furnielis1@ibm.com@Leonardos-MacBook-Pro.local>
2018-09-26 22:08:42 -0300

    model estab

---
+++ src/models/v1/establishment.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.SchemaTypes;
+
+const EstablishmentSchema = new mongoose.Schema({
+  createdAt: {
+    type: Date,
+    default: Date
+  },
+  updatedAt: {
+    type: Date
+  },
+  _user: {
+    index: true,
+    type: Schema.ObjectId,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true
+  },
+  complement: {
+    type: String,
+    required: true
+  },
+  zip: {
+    index: String,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  cnpj: {
+    type: String,
+    required: true
+  }
+}, { collection: 'establishment' });
+
+module.exports.Establishment = mongoose.model('establishment', EstablishmentSchema);
